25fef76602e4236490b6c8af141dddcfcf4cf97b
scolpig <scolpig@gmail.com>
2022-07-06 11:20:35 +0900

    word cloud 01

--- job08_movie_recommendation.py
+++ job08_movie_recommendation.py
@@ -2,6 +2,8 @@ import pandas as pd
 from sklearn.metrics.pairwise import linear_kernel
 from scipy.io import mmread
 import pickle
+from konlpy.tag import Okt
+import re
 from gensim.models import Word2Vec
 
 def getRecommendation(cosin_sim):
@@ -44,8 +46,24 @@ with open('./models/tfidf.pickle', 'rb') as f:
 # print(recommendation)
 
 # 문장 이용
+okt = Okt()
 sentence = '견딜 수 없이 촌스런 삼남매의 견딜 수 없이 사랑스러운 행복소생기'
-sentence_vec = Tfidf.transform([sentence])
+review = re.sub('[^가-힣 ]', ' ', sentence)
+
+token = okt.pos(review, stem=True)
+
+df_token = pd.DataFrame(token, columns=['word', 'class'])
+df_token = df_token[(df_token['class'] == 'Noun') |
+                    (df_token['class'] == 'Verb') |
+                    (df_token['class'] == 'Adjective')]
+words = []
+for word in df_token.word:
+
+    if 1 < len(word):
+        words.append(word)
+cleaned_sentence = ' '.join(words)
+print(cleaned_sentence)
+sentence_vec = Tfidf.transform([cleaned_sentence])
 cosine_sim = linear_kernel(sentence_vec, Tfidf_matrix)
 recommendation = getRecommendation(cosine_sim)
 print(recommendation)
---
+++ job09_word_cloud.py
@@ -0,0 +1,40 @@
+from turtle import back
+
+import background as background
+import family
+import pandas as pd
+import matplotlib.pyplot as plt
+from wordcloud import WordCloud
+import collections
+from matplotlib import font_manager, rc
+import matplotlib.pyplot as plt
+import numpy as np
+from PIL import Image
+
+font_path = './malgun.ttf'
+font_name = font_manager.FontProperties(
+    fname=font_path).get_name()
+plt.rc('font', family='NanumBarunGothic')
+
+df= pd.read_csv('./crawling_data/reviews_2017_2022.csv')
+words = df[df['titles']=='브라더 오브 더 이어 (Brother of the Year)']['reviews']
+print(words.iloc[0])
+words = words.iloc[0].split()
+print(words)
+
+worddict = collections.Counter(words)
+worddict = dict(worddict)
+print(worddict)
+
+wordcloud_img = WordCloud(
+    background_color='white', max_words=2000,
+    font_path=font_path).generate_from_frequencies(worddict)
+
+plt.figure(figsize=(12, 12))
+plt.imshow(wordcloud_img, interpolation='bilinear')
+plt.axis('off')
+plt.show()
+
+
+
+
